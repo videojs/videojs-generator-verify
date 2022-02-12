a20cb376871f480b7793dc3cfadacc44b16ba710
mister-ben <git@misterben.me>
2022-02-12 09:42:26 +0100

    Test require with peerdeps

---
+++ test/fixture/bar.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const dummy = require('dummy');
+
+console.log(dummy());
--- test/require.test.js
+++ test/require.test.js
@@ -12,6 +12,19 @@ test('can succeed', (t) => {
   });
 });
 
+test('succeeds when peerDependencies are required', (t) => {
+  t.context.pkg.peerDependencies = { dummy: 'file:' + path.resolve('./test/testpkg') };
+  t.context.pkg.main = './bar.js';
+
+  console.log(t.context.pkg.peerDependencies); // eslint-disable-line no-console
+
+  fs.writeFileSync(path.join(t.context.dir, 'package.json'), JSON.stringify(t.context.pkg));
+
+  return canRequire(t.context.dir, t.context.pkg).then(function(r) {
+    t.is(r.result, 'pass', 'success');
+  });
+});
+
 test('skipped if pkg has no main', (t) => {
   delete t.context.pkg.main;
   return canRequire(t.context.dir, t.context.pkg).then(function(r) {
